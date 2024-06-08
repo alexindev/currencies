64572c78e4b63c18c3d5b30503af3e97c0a1aa7c
alex <55256281+eruslanov-opt@yandex.ru>
2024-06-08 23:46:02 +0300

    create handlers

---
+++ app/telegram/handlers/user.py
@@ -0,0 +1,28 @@
+from aiogram import types, Dispatcher, F
+
+from app.telegram.loader import bot, db
+from app.telegram.keyboars.inline import main_kb, back_kb
+
+
+async def start_menu(message: types.Message):
+    """ Стартовое сообщение """
+    db.new_user(chat_id=message.from_user.id)
+    await bot.send_message(message.from_user.id, text="Главное меню:", reply_markup=main_kb())
+
+
+async def market_menu(callback: types.CallbackQuery):
+    """ Меню биржи """
+    await callback.answer()
+    await callback.message.edit_text("Биржи будут тут", reply_markup=back_kb())
+
+
+async def start_menu_cb(callback: types.CallbackQuery):
+    """ Стартовое сообщение c callback"""
+    await callback.answer()
+    await callback.message.edit_text(text="Выберите меню:", reply_markup=main_kb())
+
+
+def user_handlers(dp: Dispatcher):
+    dp.message.register(start_menu, F.text == '/start')
+    dp.callback_query.register(market_menu, F.data == 'market')
+    dp.callback_query.register(start_menu_cb, F.data == 'back')
