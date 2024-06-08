bc589a4497ba2d66ba6ddb3a6275dc286b1b971f
alex <55256281+eruslanov-opt@yandex.ru>
2024-06-08 23:47:01 +0300

    create db

---
+++ app/grabber/database/mongo.py
@@ -0,0 +1,77 @@
+from pymongo import MongoClient
+
+
+class MongoDBManager:
+    """ Класс для управления MongoDB """
+
+    def __init__(self, host: str = 'localhost', port: int = 27017, database_name: str = 'currencies'):
+        """
+        Инициализация соединения с MongoDB
+
+        :param host: Адрес хоста MongoDB. По умолчанию: 'localhost'
+        :param port: Порт MongoDB. По умолчанию: 27017
+        :param database_name: Имя базы данных
+        """
+        self.client = MongoClient(f'mongodb://{host}:{port}/')
+        self.db = self.client[database_name]
+        self.collection = self.db['binance']
+
+        # Создаем индекс только при первом запуске
+        if not self.collection.index_information().get("timestamp"):
+            self.collection.create_index([("timestamp", 1)], expireAfterSeconds=600, name="timestamp")
+
+    def insert_document(self, collection_name: str, document: dict):
+        """
+        Вставка документа в коллекцию
+
+        :param collection_name: Имя коллекции
+        :param document: Документ для вставки
+        """
+        collection = self.db[collection_name]
+        return collection.insert_one(document)
+
+    def find_documents(self, collection_name: str, projection=None, query=None):
+        """
+        Поиск документов в коллекции
+
+        :param collection_name: Имя коллекции
+        :param projection: Поля для выборки (по умолчанию: все поля)
+        :param query: Критерии поиска (по умолчанию: все документы)
+        """
+        if projection is None:
+            projection = {}
+        if query is None:
+            query = {}
+        collection = self.db[collection_name]
+        return collection.find(query, projection)
+
+    def find_document(self, collection_name: str, query=None):
+        """
+        Поиск одного документа в коллекции
+
+        :param collection_name: Имя коллекции
+        :param query: Критерии поиска (по умолчанию: первый документ)
+        """
+        if query is None:
+            query = {}
+        collection = self.db[collection_name]
+        return collection.find_one(query)
+
+    def update_document(self, collection_name: str, query: dict, update: dict):
+        """
+        Обновление документа в коллекции
+
+        :param collection_name: Имя коллекции
+        :param query: Критерии поиска документа для обновления
+        :param update: Обновления для применения
+        """
+        collection = self.db[collection_name]
+        return collection.update_one(query, update)
+
+
+# m = MongoDBManager()
+# for i in m.find_documents(collection_name='binance'):
+#     print(i)
+#
+# print(m.collection.delete_many({}))
+
---
+++ app/telegram/database/sqlite.py
@@ -0,0 +1,103 @@
+import sqlite3
+
+
+class SqliteDatabase:
+    def __init__(self):
+        self.path = './database/database.db'
+
+        self.conn = sqlite3.connect(database=self.path)
+        self.cursor = self.conn.cursor()
+
+        # Создаем таблицу, если она не существует
+        self.create_table()
+
+    def create_table(self):
+        """ Создает таблицу пользователей """
+        query = """
+        CREATE TABLE IF NOT EXISTS users (
+            chat_id INTEGER PRIMARY KEY,
+            long_percent INTEGER,
+            long_time INTEGER,
+            short_percent INTEGER,
+            short_time INTEGER,
+            pay_time INTEGER,
+            is_admin BOOL)
+        """
+        self.cursor.execute(query)
+        self.conn.commit()
+
+    def exists_user(self, chat_id):
+        """ Проверить существование пользователя
+        Если пользователь существует вернуть True, иначе
+        """
+        query = "SELECT 1 FROM users WHERE chat_id = ?"
+        self.cursor.execute(query, (chat_id,))
+        result = self.cursor.fetchone()
+        if result:
+            return True
+        return False
+
+    def new_user(self, chat_id: int):
+        """ Добавить пользователя """
+        exists_user = self.exists_user(chat_id)
+        if exists_user:
+            return False
+
+        # Если пользователя нет, добавляем его
+        query = "INSERT INTO users (chat_id) VALUES (?)"
+        self.cursor.execute(query, (chat_id,))
+        self.conn.commit()
+        return True
+
+    def update_user(self, chat_id: int, long_percent: int = None, long_time: int = None, short_percent: int = None,
+                    short_time: int = None):
+        """ Обновить информацию пользователя """
+
+        exists_user = self.exists_user(chat_id)
+        if not exists_user:
+            return False
+
+        update_params = []
+        update_values = []
+        if long_percent:
+            update_params.append("long_percent = ?")
+            update_values.append(long_percent)
+        if long_time:
+            update_params.append("long_time = ?")
+            update_values.append(long_time)
+        if short_percent:
+            update_params.append("short_percent = ?")
+            update_values.append(short_percent)
+        if short_time:
+            update_params.append("short_time = ?")
+            update_values.append(short_time)
+
+        # Формируем запрос
+        if update_params:
+            query = f"UPDATE users SET {', '.join(update_params)} WHERE chat_id = ?"
+            update_values.append(chat_id)
+            self.cursor.execute(query, update_values)
+            self.conn.commit()
+            return True
+        else:
+            return False
+
+    def get_user(self, chat_id: int):
+        """ Получить информацию о пользователе """
+        query = "SELECT * FROM users WHERE chat_id = ?"
+        self.cursor.execute(query, (chat_id,))
+        result = self.cursor.fetchone()
+        if result:
+            result = {
+                'long_percent': result[1],
+                'long_time': result[2],
+                'short_percent': result[3],
+                'short_time': result[4],
+                'pay_time': result[5],
+                'is_admin': result[6],
+            }
+        return result
+
+    def close_connection(self):
+        """ Закрыть соединение """
+        self.conn.close()
