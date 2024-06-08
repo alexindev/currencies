1ff9f37cf36c309241148bc1d88160e967e3d468
alex <55256281+eruslanov-opt@yandex.ru>
2024-06-08 23:48:16 +0300

    create keyboards

---
+++ app/telegram/keyboars/commands.py
@@ -0,0 +1,13 @@
+from aiogram.types import BotCommand, BotCommandScopeDefault
+from app.telegram.loader import bot
+
+
+async def set_commands():
+    """ Установка комманд"""
+    commands = [
+        BotCommand(
+            command='start',
+            description='Регистрация'
+        ),
+    ]
+    await bot.set_my_commands(commands=commands, scope=BotCommandScopeDefault())
