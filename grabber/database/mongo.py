8c6714ea33c13f599b96b0c41b6d6e61d34ef045
alex <55256281+eruslanov-opt@yandex.ru>
2024-06-17 00:54:08 +0300

    добавил новый метод для удаления

--- grabber/database/mongo.py
+++ grabber/database/mongo.py
@@ -2,22 +2,32 @@ from pymongo import MongoClient
 
 
 class MongoDBManager:
-    """ Класс для управления MongoDB """
+    """ Менеджер MongoDB """
 
-    def __init__(self, host: str = 'mongo', port: int = 27017, currencies_db: str = 'currencies',
-                 user_db: str = 'users'):
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        """" Singleton """
+        if cls._instance is None:
+            cls._instance = super(MongoDBManager, cls).__new__(cls, *args, **kwargs)
+        return cls._instance
+
+    def __init__(self, host: str = 'mongo', currencies_db: str = 'currencies', user_db: str = 'users'):
         """
         Инициализация соединения с MongoDB
 
         :param host: Адрес хоста MongoDB'
-        :param port: Порт MongoDB. По умолчанию: 27017
         :param currencies_db: Имя базы данных для бирж
         :param user_db: Имя базы данных для пользователей
         """
-        self.client = MongoClient(f'mongodb://{host}:{port}/')
+        self.client = MongoClient(f'mongodb://{host}:27017/')
+
         self.currencies_db = self.client[currencies_db]
         self.binance_collection = self.currencies_db['binance']
         self.bybit_collection = self.currencies_db['bybit']
+
+        self.user_db = self.client[user_db]
+        self.user_collection = self.user_db['profile']
 
         # Создаем индексы только при первом запуске
         if not self.binance_collection.index_information().get("timestamp"):
@@ -26,12 +36,9 @@ class MongoDBManager:
         if not self.bybit_collection.index_information().get("timestamp"):
             self.bybit_collection.create_index([("timestamp", 1)], expireAfterSeconds=600, name="timestamp")
 
-        self.user_db = self.client[user_db]
-        self.user_collection = self.user_db['users']
-
-    def insert_document(self, collection_name: str, document: dict, db_name: str = 'currencies'):
+    def insert_document(self, document: dict, collection_name: str, db_name: str = 'currencies'):
         """
-        Вставка документа в коллекцию
+        Вставка документа
 
         :param collection_name: Имя коллекции
         :param document: Документ для вставки
@@ -44,17 +51,14 @@ class MongoDBManager:
         collection = db[collection_name]
         return collection.insert_one(document)
 
-    def find_documents(self, collection_name: str, projection=None, query=None, db_name: str = 'currencies'):
+    def find_documents(self, collection_name: str, db_name: str = 'currencies', query=None):
         """
-        Поиск документов в коллекции
+        Поиск документов
 
         :param collection_name: Имя коллекции
-        :param projection: Поля для выборки (по умолчанию: все поля)
-        :param query: Критерии поиска (по умолчанию: все документы)
         :param db_name: Имя базы данных
+        :param query: Критерии поиска (по умолчанию: все документы)
         """
-        if projection is None:
-            projection = {}
         if query is None:
             query = {}
         if db_name == 'currencies':
@@ -62,11 +66,11 @@ class MongoDBManager:
         else:
             db = self.user_db
         collection = db[collection_name]
-        return collection.find(query, projection)
+        return collection.find(query)
 
     def find_document(self, collection_name: str, query=None, db_name: str = 'currencies'):
         """
-        Поиск одного документа в коллекции
+        Поиск одного документа
 
         :param collection_name: Имя коллекции
         :param query: Критерии поиска (по умолчанию: первый документ)
@@ -83,7 +87,7 @@ class MongoDBManager:
 
     def update_document(self, collection_name: str, query: dict, update: dict, db_name: str = 'currencies'):
         """
-        Обновление документа в коллекции
+        Обновление документа
 
         :param collection_name: Имя коллекции
         :param query: Критерии поиска документа для обновления
@@ -96,3 +100,29 @@ class MongoDBManager:
             db = self.user_db
         collection = db[collection_name]
         return collection.update_one(query, update)
+
+    def delete_document(self, query: dict, collection_name: str, db_name: str = 'currencies'):
+        """
+        Удаление документа
+
+        :param query: Критерии для удаления
+        :param db_name: Имя базы данных
+        :param collection_name: Имя коллекции
+        """
+        if db_name == 'currencies':
+            db = self.currencies_db
+        else:
+            db = self.user_db
+        collection = db[collection_name]
+        return collection.delete_one(query)
+
+    def set_user_settings(self, chat_id: int, document: dict):
+        """ Добавить настройки пользователя """
+        self.user_collection.update_one(
+            {'chat_id': chat_id},
+            {'$set': document},
+            upsert=True
+        )
+
+
+mongo_client = MongoDBManager()
